1af9a4f91473a0c9cb997fe9f0a5a786fa398d07
emiliodes <104631473+emiliodes@users.noreply.github.com>
2025-03-09 02:45:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var testimonialCarousel = new bootstrap.Carousel('#testimonialCarousel', {
+        interval: 3000, // Change slide every 3 seconds
+        ride: 'carousel'
+    });
+});
